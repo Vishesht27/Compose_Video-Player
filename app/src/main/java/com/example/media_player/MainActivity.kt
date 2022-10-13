5498295376d8009df2c8d03d3c61fac14ca032d2
Vishesh <vishesht27@gmail.com>
2022-10-13 12:43:16 +0530

    Added dependencies

--- app/src/main/java/com/example/media_player/MainActivity.kt
+++ app/src/main/java/com/example/media_player/MainActivity.kt
@@ -17,28 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Media_PlayerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
 
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Media_PlayerTheme {
-        Greeting("Android")
     }
 }
